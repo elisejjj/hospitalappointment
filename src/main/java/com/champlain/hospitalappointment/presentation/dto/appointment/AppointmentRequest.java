7be6cc2f59c78a4950ecd08c93e6a7d4fd8c8307
jjjelise <jjjelise@gmail.com>
2025-11-10 21:33:30 -0500

    created AppointmentRequest

--- src/main/java/com/champlain/hospitalappointment/presentation/dto/appointment/AppointmentRequest.java
+++ src/main/java/com/champlain/hospitalappointment/presentation/dto/appointment/AppointmentRequest.java
@@ -1,4 +1,9 @@
 package com.champlain.hospitalappointment.presentation.dto.appointment;
-
-public class AppointmentRequest {
-}
+import jakarta.validation.constraints.*;
+public record AppointmentRequest(
+        @NotBlank String brand,
+        @NotBlank String model,
+        @NotBlank String color,
+        @NotNull @Positive Long patientId,
+        @NotNull @Positive Long doctorId
+) {}
