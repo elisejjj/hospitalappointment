ca3e179e3feb77b6015ea74c9b39640d3050984e
jjjelise <jjjelise@gmail.com>
2025-11-10 21:43:40 -0500

    created DoctorRequest and PatientRequest

--- src/main/java/com/champlain/hospitalappointment/presentation/dto/doctor/DoctorRequest.java
+++ src/main/java/com/champlain/hospitalappointment/presentation/dto/doctor/DoctorRequest.java
@@ -1,4 +1,11 @@
 package com.champlain.hospitalappointment.presentation.dto.doctor;
-
-public class DoctorRequest {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+public record DoctorRequest(
+        @NotBlank @Size(max=80) String firstName,
+        @NotBlank @Size(max=80) String lastName,
+        @NotBlank @Email String email,
+        @Size(max=40) String phone
+) {
 }
