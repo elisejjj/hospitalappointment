65c84d6448370790d3343c63167a52790db0e6f7
jjjelise <jjjelise@gmail.com>
2025-11-10 22:06:09 -0500

    created DoctorMapper

--- src/main/java/com/champlain/hospitalappointment/presentation/mapper/DoctorMapper.java
+++ src/main/java/com/champlain/hospitalappointment/presentation/mapper/DoctorMapper.java
@@ -1,4 +1,33 @@
 package com.champlain.hospitalappointment.presentation.mapper;
 
-public class DoctorMapper {
+import com.champlain.hospitalappointment.dataaccess.entity.Doctor;
+import com.champlain.hospitalappointment.presentation.dto.doctor.DoctorRequest;
+import com.champlain.hospitalappointment.presentation.dto.doctor.DoctorResponse;
+
+import javax.print.Doc;
+
+public final class DoctorMapper {
+    private DoctorMapper() {
+    }
+    public static Doctor toEntity(DoctorRequest req) {
+        return Doctor.builder()
+                .firstName(req.firstName())
+                .lastName(req.lastName())
+                .email(req.email())
+                .phone(req.phone())
+                .build();
+    }
+    public static DoctorResponse toResponse(Doctor doctor) {
+        long count = doctor.getAppointments() == null ? 0 : doctor.getAppointments().size();
+        return new DoctorResponse(
+                doctor.getId(),
+                doctor.getFirstName(),
+                doctor.getLastName(),
+                doctor.getEmail(),
+                doctor.getPhone(),63
+                doctor.getCreatedAt(),
+                doctor.getUpdatedAt(),
+                count
+        );
+    }
 }
